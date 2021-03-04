d303d2b29a4e1eb24e7c76e54455e5de1f9ea7de
AndrewMedvedev42 <sanjino56@gmail.com>
2021-03-04 17:20:51 +0200

    Updates

---
+++ precache-manifest.0c87fd46cc0842735f65ede97ff573e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66eb4dc549120e385a6bbb4f1433f092",
+    "url": "/Dark-Mode-Changer/index.html"
+  },
+  {
+    "revision": "4c1f1de1c8894252efb3",
+    "url": "/Dark-Mode-Changer/static/css/main.b3fc9c3b.chunk.css"
+  },
+  {
+    "revision": "4646610705680ce7d806",
+    "url": "/Dark-Mode-Changer/static/js/2.a99e8b18.chunk.js"
+  },
+  {
+    "revision": "849657740f66d00720db130cc1e94c97",
+    "url": "/Dark-Mode-Changer/static/js/2.a99e8b18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c1f1de1c8894252efb3",
+    "url": "/Dark-Mode-Changer/static/js/main.9016795a.chunk.js"
+  },
+  {
+    "revision": "993a13aa14d8932dea73",
+    "url": "/Dark-Mode-Changer/static/js/runtime-main.29c4d053.js"
+  }
+]);
